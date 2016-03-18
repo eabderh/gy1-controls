d190d326b12963bed2a4173796d97b4e49a701dd
Joshua Johnston <josh@181.234.21.198.tigernet.wifi.dyn.clemson.edu>
2016-03-18 14:35:44 -0400

    add basic get request to server btn

--- client/public/scripts/main.js
+++ client/public/scripts/main.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
-  
+  $('#connectbtn').click(
+	  	function () {
+	      makeRequest();
+	    }            
+    );
 });
+
+function makeRequest(){
+	var xhr = new XMLHttpRequest();
+	var url ='http://requestb.in/10i5w2v1'
+	//maybe http://172.31.57.1:8080/
+	xhr.open("GET", url);
+	xhr.send();
+}
